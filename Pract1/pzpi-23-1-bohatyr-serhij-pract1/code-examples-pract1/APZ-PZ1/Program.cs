7efe7b50af5095e3e2b73ce3b01aaa643d756bf3
NureBohatyrSerhii <serhii.bohatyr@nure.ua>
2026-04-21 05:59:33 +0300

    Upload of PZ1

---
+++ Pract1/pzpi-23-1-bohatyr-serhij-pract1/code-examples-pract1/APZ-PZ1/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace APZ_PZ1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Restaurant sushiRestaurant = new SushiRestaurant();
+            sushiRestaurant.PlanOrder();
+
+            Restaurant pizzaRestaurant = new PizzaRestaurant();
+            pizzaRestaurant.PlanOrder();
+        }
+    }
+
+    public interface IFood
+    {
+        void Order();
+    }
+
+    public class Sushi : IFood
+    {
+        public void Order() 
+        {
+            Console.WriteLine("Замовлено суші");
+        }
+    }
+
+    public class Pizza : IFood
+    {
+        public void Order()
+        {
+            Console.WriteLine("Замовлено піцу");
+        }
+    }
+
+    public abstract class Restaurant
+    {
+        public abstract IFood CreateFood();
+
+        public void PlanOrder()
+        {
+            IFood food = CreateFood();
+            food.Order();
+        }
+    }
+
+    public class SushiRestaurant : Restaurant
+    {
+        public override IFood CreateFood() => new Sushi();
+    }
+
+    public class PizzaRestaurant : Restaurant
+    {
+        public override IFood CreateFood() => new Pizza();
+    }
+}
